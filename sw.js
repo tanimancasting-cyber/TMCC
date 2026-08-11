c47737928bc8ec18d4a7098885376570642601da
tanimancasting-cyber <tanimancasting@gmail.com>
2026-08-11 09:07:41 +0530

    CREATE TMCC APP

--- sw.js
+++ sw.js
@@ -1,30 +1,15 @@
 const CACHE_NAME = "tmcc-v1";
 
-const FILES_TO_CACHE = [
-  "/TMCC/",
-  "/TMCC/index.html",
-  "/TMCC/logo.png",
-  "/TMCC/manifest.json"
-];
-
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(cache => cache.add("/TMCC/"))
       .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
